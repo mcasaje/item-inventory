dd532cedb11d76df4aaf97e3f41e3b210c9e0162
Mark Casaje <mcasaje@gmail.com>
2015-09-05 20:20:22 -0700

    Bug Fix: Hibernate no longer creates tables. Evolutions now handles table creation.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,15 +1,10 @@
 # --- !Ups
 
 CREATE TABLE app_user (
-  id int NOT NULL AUTO_INCREMENT,
+  id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   username VARCHAR(100) NOT NULL,
   password_salt VARCHAR(100) NOT NULL,
   password_hash VARCHAR(100) NOT NULL,
   PRIMARY KEY (id),
   UNIQUE INDEX uq_app_users_username(username)
 );
-
-
-# --- !Downs
-
-DROP TABLE app_user;
